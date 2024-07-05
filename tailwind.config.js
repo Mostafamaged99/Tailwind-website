55d4804439e6293bd1eaa1558e1b9587cff0d245
mostafamaged99 <mostafamaged99>
2024-07-05 11:30:51 +0300

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        main: "url('./imgs/pexels-tracehudson-2529973.jpg')"
+      }
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
